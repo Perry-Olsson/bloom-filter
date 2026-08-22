a534cec0879d76e7b3c865ecf035250fa4fc7a45
Perry Olsson <olsson.perry@gmail.com>
2026-08-22 11:13:08 -0700

    query function

--- tests/it_test.rs
+++ tests/it_test.rs
@@ -1,11 +1,23 @@
 use std::{collections::HashMap, sync::LazyLock, time::Instant};
 use deepsize::DeepSizeOf;
+use rand::{rngs::ThreadRng, seq::SliceRandom, thread_rng, Rng};
 
 #[test]
 fn test_hash_map() {
     let (map, duration, memory) = measure(|| build_hash_map(2000));
     println!("HashMap build: {:?} | memory: {} bytes", duration, memory);
     assert!(!map.is_empty());
+
+    let hits = 10;
+    let misses = 10;
+    let (found, query_duration, _) = measure(|| query_hash_map(&map, hits, misses));
+    println!(
+        "HashMap query: {} found ({} hit attempts + {} misses) | duration: {:?}",
+        found,
+        hits,
+        misses,
+        query_duration
+    );
 }
 
 fn build_hash_map(username_count: usize) -> HashMap<String, usize> {
@@ -16,6 +28,43 @@ fn build_hash_map(username_count: usize) -> HashMap<String, usize> {
         *map.entry(username).or_insert(0) += 1;
     }
     map
+}
+
+fn query_hash_map(
+    map: &HashMap<String, usize>,
+    hits: usize,
+    misses: usize,
+) -> usize {
+    let mut rng = thread_rng();
+    let mut hits_count = hits;
+    let mut misses_count = misses;
+
+    let mut found = 0;
+    while hits_count > 0 || misses_count > 0 {
+        let val: f64 = rng.gen_range(0.0..=1.0);
+        let get_value: fn(&mut ThreadRng) -> String;
+        if hits_count != 0 && (val > 0.5 || misses_count == 0) {
+            get_value = get_hit;
+            hits_count -= 1;
+        } else  {
+            get_value = get_miss;
+            misses_count -= 1;
+        }
+
+        let val = &get_value(&mut rng);
+        if map.contains_key(val) {
+            found += 1;
+        }
+    }
+    found
+}
+
+fn get_hit<T: Rng>(rng: &mut T) -> String {
+    USERNAMES.choose(rng).unwrap().clone() + "_0"
+}
+
+fn get_miss<T: Rng>(rng: &mut T) -> String {
+    (0..10).map(|_| rng.gen_range('a'..='z')).collect()
 }
 
 struct UsernameGenerator {
